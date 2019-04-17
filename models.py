fix shape calculation

models.py
@@ -123,11 +123,7 @@ if torch.cuda.is_available():
 else:
     device = 'cpu'
 
-n_inputs = 8
-shape = n_inputs * 2
-span = 2
-layers = (n_inputs - 2) // (span * 2) + 1
-model = ASIC((shape,), layers, (span,), device)
+model = ASIC((16,), 5, (2,), device)
 
 def f(x):
     ret = abs((x[:, 1] * x[:, 0]).unsqueeze(-1) - x)