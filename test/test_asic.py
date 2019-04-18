a475a3e4440c294357bc20ed68232401adb473da
Alex Eftimiades <alexeftimiades@gmail.com>
2019-04-18 18:34:32 -0500

    add tests for embed

--- test/test_asic.py
+++ test/test_asic.py
@@ -5,7 +5,6 @@ from models import ASIC
 
 def test_convolve():
     model = ASIC((8,), 1, (2,), 'cpu')
-    batch_size = 1
     x = torch.from_numpy(numpy.asarray([[0,1,2,3,4,5,6,7]]))
     convolved = model.convolve(x)
     assert torch.all(torch.eq(convolved, torch.from_numpy(
@@ -18,3 +17,15 @@ def test_convolve():
                 [3,4,5,6,7],
                 [4,5,6,7,0],
                 [5,6,7,0,1]]]))))
+
+def test_embed():
+    x = torch.from_numpy(numpy.asarray([[2,2,2,2,2,2,2,2]])).float()
+    model = ASIC((8,), 1, (2,), 'cpu')
+    slices = model.embed(x)
+    assert torch.all(model.state == 2)
+    model = ASIC((16,), 1, (2,), 'cpu')
+    slices = model.embed(x)
+    assert torch.all(model.state[:, ::2] == 2)
+    model = ASIC((12,), 1, (2,), 'cpu')
+    slices = model.embed(x)
+    assert torch.all(model.state[:, torch.from_numpy(numpy.asarray([1, 2, 4, 5, 7, 8]))] == 2)
